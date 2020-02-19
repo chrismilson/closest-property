7f0c28bc22d8504f257a89e850d33dd6484c2029
Chris Milson <chris.w.milson@gmail.com>
2020-02-19 15:22:41 +0900

    Lint and format

--- src/__tests__/closest.test.ts
+++ src/__tests__/closest.test.ts
@@ -1,20 +1,22 @@
-import closest from '../'
+import closest from '../';
 
-interface Dictionary { [key: string]: string }
+interface Dictionary {
+  [key: string]: string;
+}
 
 const things: Dictionary = {
   house: 'This is a house.',
   car: 'This is a car.',
-  dog: 'This is a dog.'
-}
+  dog: 'This is a dog.',
+};
 
 const bestCaseTimeComplexity: Dictionary = {
   BUBBLE_SORT: 'O(n^2)',
-  QUICK_SORT: 'O(log(n))'
-}
+  QUICK_SORT: 'O(log(n))',
+};
 
 test('Members with the same name should be chosen.', () => {
   Object.keys(things).forEach(key => {
-    expect(closest(things, key)).toBe(things[key])
-  })
-})
+    expect(closest(things, key)).toBe(things[key]);
+  });
+});
